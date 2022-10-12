430011214c6c14f275df29ef2314ed311a681269
Gabriel Aguirre Flaska <gabrielflaska.dvl@gmail.com>
2022-10-12 19:00:54 -0300

    agregando metodos vistos en la clase

--- claseConectar.py
+++ claseConectar.py
@@ -27,10 +27,67 @@ class Conectar():
             except mysql.connector.Error as descripcionErrorListarAlbumes:
                 print("Error connecting to database", descripcionErrorListarAlbumes)
 
+    def ListarTema(self):
+        if self.conexion.is_connected():
+            try:
+                cursor = self.conexion.cursor()
+                cursor.execute("SELECT * from tema")
+                resultados = cursor.fetchall()
+                self.conexion.close()
+                return resultados
+
+            except mysql.connector.Error as descripcionErrorListarAlbumes:
+                print("Error connecting to database", descripcionErrorListarAlbumes)
+
+    def InsertarInterprete(self, nombre, apellido, nacionalidad, foto):
+        if self.conexion.is_connected():
+            try:
+                cursor = self.conexion.cursor()
+                sentenciaSQL = "INSERT into interprete values(null, %s, %s, %s, %s)"
+
+                data = (nombre, apellido,nacionalidad,foto)
+
+                cursor.execute(sentenciaSQL, data)
+
+                self.conexion.commit()
+                self.conexion.close()
+                print ("Interprete intersatado correctamente")
+
+            except mysql.connector.Error as descripcionErrorListarAlbumes:
+                print("Error connecting to database", descripcionErrorListarAlbumes)
+
+    def InsertarGenero(self, nombre):
+        if self.conexion.is_connected():
+            try:
+                cursor = self.conexion.cursor()
+                sentenciaSQL = "INSERT into genero values(null, %s)"
+
+                data = (nombre,)
+
+                cursor.execute(sentenciaSQL, data)
+
+                self.conexion.commit()
+                self.conexion.close()
+                print ("Genero intersatado correctamente")
+
+            except mysql.connector.Error as descripcionErrorListarAlbumes:
+                print("Error connecting to database", descripcionErrorListarAlbumes)
+
+
+
+
+
+
+#Insertar datos a interprete
+# con = Conectar()
+# con.InsertarInterprete("Gabriel", "Aguirre", "Argentina","")
+
 #Listado de albumes disponibles por artista, en orden alfabetico
-
 #con = Conectar()
-
-#for album in con.ListarAlbumes():
+#for album in con.ListarTema():
 #    print ("Album:", album[1] + ". " + "Interprete:", album[2])
 
+#Listado de temas
+#con = Conectar()
+#for tema in con.ListarTema():
+#    print ("tema:", tema[1])
